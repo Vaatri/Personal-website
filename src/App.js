efab52c06b049e01a8fd5873cdb289faac68779a
Van-Roy Trinh <z5163773@ad.unsw.edu.au>
2021-04-30 15:01:28 +1000

    changed title

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import EntryPage from './pages/EntryPage';
 import { BrowserRouter as Router } from 'react-router-dom';
 import AboutMe from './pages/AboutMe';
 import ProjectLinks from './pages/ProjectLinks';
+import React from 'react';
 
 function App() {
+  React.useEffect(() => {
+    document.title = "Van-Roy Trinh";
+  },[]);
+  
   return (
     <Router>
       <EntryPage />
